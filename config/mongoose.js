87693d46a0f2c395cf414f76a5bae3677209f288
Piyaphat Pinyo <remove158@gmail.com>
2021-08-02 01:13:51 +0700

    ✅ add : .env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
+// config/mongoose.js
 import { mongoUri, debug } from "./config.js";
 import mongoose from "mongoose";
 export default function () {
 	mongoose.set("debug", debug);
-	console.log("🚀 ~ file: mongoose.js ~ line 8 ~ mongoUri", mongoUri);
 	const db = mongoose.connect(mongoUri).catch((e) => {
 		console.log("Can't connect to database.");
 	});
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// server.js
+
 import express from "./config/express.js";
 import mongoose from "./config/mongoose.js";
-const db = mongoose();
+// เรียกใช้ db ก่อนเพื่อต่อ mongoose  ( ป้องกัน express ต่อไม่ติด)
+const db = mongoose(); // return mongoose.connect(config.mongoUri)
 const app = express();
 app.listen(3000, () => {
 	console.log("Server listenning on port 3000!");
